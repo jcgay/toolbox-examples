43fc7bd2e46862d1005acd1449a382a4ddd6d1ec
Jean-Christophe GAY <contact@jeanchristophegay.com>
2014-03-30 10:56:48 +0200

    Consumer.

---
+++ java-8/src/main/java/fr/jcgay/example/java/Consumer/Examples.java
@@ -0,0 +1,15 @@
+package fr.jcgay.example.java.Consumer;
+
+import java.util.function.Consumer;
+
+public class Examples {
+
+    public static void main(String[] args) {
+
+        Consumer<String> print = s -> System.out.println(s);
+        Consumer<String> hello = s -> System.out.printf("Hello %s !", s);
+
+        print.accept("something"); // something
+        print.andThen(hello).accept("JC"); // JC Hello JC !
+    }
+}
