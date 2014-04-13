b287ca913c164d3b48133874333dab3ac2482e4f
Jean-Christophe GAY <contact@jeanchristophegay.com>
2014-04-13 16:23:29 +0200

    BiFunction, UnaryOperator, BinaryOperator.

--- java-8/src/main/java/fr/jcgay/example/java/function/Examples.java
+++ java-8/src/main/java/fr/jcgay/example/java/function/Examples.java
@@ -1,6 +1,9 @@
 package fr.jcgay.example.java.function;
 
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class Examples {
 
@@ -13,6 +16,14 @@ public class Examples {
 
         assert "4".equals(toString.compose(toInteger).apply("4"));
         assert toString.andThen(toInteger).apply(4) == 4;
+
+        BiFunction<Integer, String, String> concat = (Integer i, String s) -> s + ": " + i;
+        assert "un: 1".equals(concat.apply(1, "un"));
+
+        assert UnaryOperator.identity().apply("un").equals("un");
+
+        BinaryOperator<String> concatString = (s1, s2) -> s1.concat(": ").concat(s2);
+        assert "un: 1".equals(concatString.apply("un", "1"));
     }
 
 }
