947347bc26dd4a112b8b16734482d69e6b01ad5d
Jean-Christophe GAY <contact@jeanchristophegay.com>
2015-05-29 13:32:54 +0200

    Testing exceptions

---
+++ spock/src/test/groovy/ExceptionSpec.groovy
@@ -0,0 +1,29 @@
+import spock.lang.Specification
+
+class ExceptionSpec extends Specification {
+
+    def "should not throw exception"() {
+        when:
+        [1].get(0)
+
+        then:
+        noExceptionThrown()
+    }
+
+    def "should not throw IndexOutOfBoundsException"() {
+        when:
+        [1].get(0)
+
+        then:
+        notThrown(IndexOutOfBoundsException)
+    }
+
+    def "should throw exception"() {
+        when:
+        [1].get(1)
+
+        then:
+        IndexOutOfBoundsException e = thrown()
+        e.message == 'Index: 1, Size: 1'
+    }
+}
